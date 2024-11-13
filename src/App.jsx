11fd2b9ea0269d7010fe8828574da5ba52f8fada
DevFarhanCoder <154915648+DevFarhanCoder@users.noreply.github.com>
2024-11-13 14:40:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
+
+export default function App() {
+  const [category, setCategory] = useState("business");
+  const [country, setCountry] = useState("us");
+  const [mode, setMode] = useState("light");
+  const [progress, setProgress] = useState(10);
+  const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+
+  function handleCategoryChange(category) {
+    setCategory(category);
+  }
+
+  function handleCountryChange(country) {
+    setCountry(country);
+  }
+  function switchMode() {
+    if (mode == "light") {
+      setMode("dark");
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+    } else {
+      setMode("light");
+      document.documentElement.setAttribute("data-bs-theme", "light");
+    }
+  }
+  function changeProgress(num) {
+    setProgress(num);
+  }
+
+  useEffect;
+  return (
+    <>
+      <div>
+        <LoadingBar
+          color="#f11946"
+          progress={progress}
+          height={3}
+          onLoaderFinished={() => setProgress(0)}
+        />
+      </div>
+      <Navbar
+        setCategory={handleCategoryChange}
+        setCountry={handleCountryChange}
+        country={country}
+        toggleMode={switchMode}
+        mode={mode}
+      ></Navbar>
+      <News
+        pageSize={15}
+        country={country}
+        category={category}
+        mode={mode}
+        changeProgress={changeProgress}
+        apiKey={apiKey}
+      ></News>
+    </>
+  );
+}
